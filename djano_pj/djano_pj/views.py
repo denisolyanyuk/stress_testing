854e55728dee5aeaafbef24cee765dc84f8fb642
denis <denis.o@hugohunter.com>
2021-11-12 03:07:17 +0200

    init

--- djano_pj/djano_pj/views.py
+++ djano_pj/djano_pj/views.py
@@ -30,19 +30,18 @@ def create_all_users(request):
 
 def get_random_user(request):
     customer_random_id = random.randint(0, customers_amount-1)
-    # redis_customer_encoded = redis_connection.get(customer_random_id)
-    # if redis_customer_encoded:
-    #     ttl = redis_connection.pttl(customer_random_id) / 1000
-    #     if not should_flush_cache(ttl, cache_flushing_threshold_ttl):
-    #         pass
-    #     redis_customer = redis_customer_encoded.decode()
-    #     customer = json.loads(redis_customer)
-    #     return JsonResponse(customer)
+    redis_customer_encoded = redis_connection.get(customer_random_id)
+    if redis_customer_encoded:
+        ttl = redis_connection.pttl(customer_random_id) / 1000
+        if not should_flush_cache(ttl, cache_flushing_threshold_ttl):
+            redis_customer = redis_customer_encoded.decode()
+            customer = json.loads(redis_customer)
+            return JsonResponse(customer)
 
     customer = customers.find_one({"cust_id": f"{customer_random_id}"})
     customer.pop('_id')
-    # customer_string = json.dumps(customer)
-    # redis_connection.setex(customer_random_id, redis_ttl, customer_string)
+    customer_string = json.dumps(customer)
+    redis_connection.setex(customer_random_id, redis_ttl, customer_string)
     return JsonResponse(customer)
 
 
